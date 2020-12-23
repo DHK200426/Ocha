7144fa8d585b610bdd7371749b2ae000c3880766
DHK200426 <39899853+DHK200426@users.noreply.github.com>
2020-12-23 19:31:54 +0900

    Add files via upload

--- manyrock.m
+++ manyrock.m
@@ -1,7 +1,8 @@
 function Maxflot = manyrock(n)
 t = 0;
 syms x;
-expr = log(x^exp(1)*x);
+
+expr = log(3*x + 2);  %이곳에 원하는 수식 삽입
 F = int(expr,[0,1]);
 
 f = double(F);
@@ -9,9 +10,9 @@ f = double(F);
 while t < n
     t = t+1;
     G = rock(t);
-    Ocha2 = ((f - G)/f)*100;
+    Ocha2 = ((f - G)/f)*100;   
     
-    Maxflot(t) = Ocha2;
+    Maxflot(t) = Ocha2; %ignore error
 end
 plot(Maxflot);
 end
--- rock.m
+++ rock.m
@@ -4,7 +4,7 @@ t = 0;
 sumintral = 0;
 intergralnew = 0;
 syms x;
-expr = log(x^exp(1)*x);
+expr = log(3*x + 2); %이곳에 원하는 수식 삽입
 F = int(expr,[0,1]);
 
 f = double(F);
@@ -16,7 +16,6 @@ while t<n
      h = t/n;
      S = subs(expr,h);
      s = double (S);
-     s = abs(s);
      A = s*k;
      intergralnew = A;
 end
@@ -25,7 +24,6 @@ if t == n
     k = 1/n;
     S = subs(expr,1);
     s = double (S);
-    s = abs(s);
     A = s * k;
     
     intergral2 = sumintral + A;
